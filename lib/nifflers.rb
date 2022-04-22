7e4697ea7d82cfaefb92887fd52a3fdf00fc3297
Josh Holtz <me@joshholtz.com>
2022-04-21 23:19:48 -0500

    Pre proof of concept

--- lib/nifflers.rb
+++ lib/nifflers.rb
@@ -2,10 +2,45 @@
 
 require_relative "nifflers/version"
 
+require 'pp'
+
 module Nifflers
   class CLI
     def self.start
-      print("start")
+      path = ARGV[0] || "."
+      extension = ARGV[1].strip
+
+      Dir.chdir(path) do
+        files = `git diff --name-only $(git merge-base master HEAD)`.lines.map(&:strip)
+
+        test_files = find_tests(files, extension)
+
+        files_spaced = test_files.join(" ")
+
+        cmd = "(cd #{path} && bundle exec rspec #{files_spaced})"
+        puts cmd
+        Kernel.exec(cmd)
+      end
+    end
+
+    def self.find_tests(files, ext)
+      test_files = []
+
+      files.each do |file|
+        basename = File.basename(file)
+        name = File.basename(file, ".*")
+        extension = File.extname(file)
+
+        next if extension != ".#{ext}"
+
+        test_files += Dir["**/#{name}*_spec#{extension}"]
+
+        if !file.end_with?("_spec.rb")
+          test_files.delete(file)
+        end
+      end
+
+      return test_files.uniq
     end
   end
   
