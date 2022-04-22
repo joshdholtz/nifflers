a01d9cf373680152dbe0ca03909c117a51ecc81b
Josh Holtz <me@joshholtz.com>
2022-04-21 22:29:03 -0500

    Initial commit

---
+++ lib/nifflers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "nifflers/version"
+
+module Nifflers
+  class CLI
+    def self.start
+      print("start")
+    end
+  end
+  
+  class Error < StandardError; end
+  # Your code goes here...
+end
